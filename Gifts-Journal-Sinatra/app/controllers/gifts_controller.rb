d599004bf80f0313a435daaecaaa9fd2e0985a95
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-26 18:30:12 +0000

    finished controllers rough draft layout, need to test in shotgun

--- Gifts-Journal-Sinatra/app/controllers/gifts_controller.rb
+++ Gifts-Journal-Sinatra/app/controllers/gifts_controller.rb
@@ -3,7 +3,7 @@ class GiftsController < ApplicationController
   get '/gifts' do
     if logged_in?
        @gifts = Gift.all
-       erb :'gifts/gifts'
+       erb :'/gifts/gifts'
      else
        redirect to '/login'
      end
@@ -22,9 +22,9 @@ class GiftsController < ApplicationController
       if params[:name] == "" || params[:description] == ""
         redirect to '/gifts/create_gift'
       else
-        @gift = current_user.gifts.create(name: params[:name], description: [:description])
+        @gift= current_user.gifts.build(name: params[:name], description: params[:description])
         if @gift.save
-          redirect to '/gifts/#{@gift.id}'
+          redirect to "/gifts/#{@gift.id}"
         else
           redirect to '/gifts/create_gift'
         end
@@ -34,6 +34,62 @@ class GiftsController < ApplicationController
     end
   end
 
-  
+  get '/gifts/:id' do
+    if logged_in?
+      @gift = Gift.find_by(params[:id])
+      erb :'/gifts/show_gift'
+    else
+      redirect to '/users/login'
+    end
+  end
+
+  get '/gifts/:id/edit' do
+    if logged_in?
+      @gift = Gift.find_by_id(params[:id])
+      if @gift && @gift.user = current_user
+        erb :'gifts/edit_gift'
+      else
+        redirect to '/gifts'
+      end
+    else
+      redirect to '/users/login'
+    end
+  end
+
+  patch '/gifts/:id' do
+    if logged_in?
+      if params[:name] = "" || params[:description] = ""
+        redirec to "/gifts/#{params[:id]}/edit"
+      else
+        @gift = Gift.find_by_id(params[:id])
+        if @gift && @gift.user == current_user
+          if @gift.update(params[:name], params[:description])
+            redirect to "/gifts/#{@gift.id}"
+          else
+            redirect to "gifts/#{@gift.id}/edit"
+         end
+        else
+          redirect to '/gifts'
+         end
+       end
+    else
+      redirect to '/users/login'
+    end
+  end
+
+
+  get '/gifts/:id/delete' do
+    if logged_in?
+     @gift = Gift.find_by_id(params[:id])
+     if @gift && @gift.user == current_user
+       @gift.delete
+     end
+     redirect to '/gifts'
+   else
+     redirect to '/users/login'
+   end
+ end
+
+
 
 end
