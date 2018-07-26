3a535a0f0e45e863597aa3773c6a6f43cb1d76ea
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-26 00:41:23 +0000

    add signup view

--- Gifts-Journal-Sinatra/app/controllers/users_controller.rb
+++ Gifts-Journal-Sinatra/app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  get '/users/signup' do
+    if !logged_in?
+       erb :'users/signup', locals: {message: "Please sign up before you sign in"}
+     else
+       redirect to '/gifts'
+     end
+  end
+
 end
