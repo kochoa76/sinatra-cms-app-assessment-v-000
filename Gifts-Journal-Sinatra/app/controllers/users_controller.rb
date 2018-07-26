a787509c193eccd20ab1347a217c6ce1d62f105a
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-26 15:24:16 +0000

    add more views have finished all views for both users and gifts

--- Gifts-Journal-Sinatra/app/controllers/users_controller.rb
+++ Gifts-Journal-Sinatra/app/controllers/users_controller.rb
@@ -1,24 +1,52 @@
 class UsersController < ApplicationController
 
-  get '/signup' do
+  get '/create_user' do
     if !logged_in?
-       erb :'users/signup', locals: {message: "Please sign up before you sign in"}
+       erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
      else
        redirect to '/gifts/gifts'
      end
 
-    erb :'users/signup'
+    erb :'users/create_user'
   end
 
-  post '/signup' do
+  post '/create_user' do
     if params[:username]== "" || params[:email]== "" || params[:password]== ""
-      redirect to 'users/signup'
+      redirect to 'users/create_user'
     else
       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       @user.save
       session[:user_id]= @user.id
       redirect to '/gifts/gifts'
     end
-  end 
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      redirect to '/gifts'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by_id(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id]= user.id
+      redirect to '/gifts'
+    else
+      redirect to '/create_user'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/users/login'
+    else
+      redirec to '/'
+    end
+  end
+
 
 end
