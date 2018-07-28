796bc9b5e3fe94bd4b775eb498282d14228bc4de
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-28 00:20:45 +0000

    debugging using sinatra , working on fixing logout and login views

--- Gifts-Journal-Sinatra/app/controllers/users_controller.rb
+++ Gifts-Journal-Sinatra/app/controllers/users_controller.rb
@@ -1,66 +1,76 @@
 class UsersController < ApplicationController
 
 
-  get "/users" do
-    if logged_in?
-      @users = User.all
-      erb :"/users/index"
-    else
-      flash[:message] = "You need to login to view users."
-      redirect '/'
+    get "/users/" do
+      if logged_in?
+        @users = User.all
+        erb :"users/index"
+      else
+        flash[:message] = "You need to login to view users."
+        redirect '/'
+      end
     end
+
+    get '/signup' do
+      if !logged_in?
+         erb :'users/create_user'
+       else
+         redirect to '/users'
+       end
+     end
+
+    post '/users' do
+      if params[:user][:username]== "" || params[:user][:email]== "" || params[:user][:password]== ""
+        flash[:message] = "Please, fill in all the boxes."
+        redirect to '/signup'
+      else
+        @user = User.create(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+        @user.save
+        session[:user_id]= @user.id
+         flash[:message] = "You have successfully signed up."
+        redirect to '/users'
+      end
+    end
+
+    get '/login' do
+      if !logged_in?
+     erb :'users/login'
+     else
+       redirect "/users/"
+     end
   end
 
-  get '/signup' do
-    if !logged_in?
-       erb :'users/create_user'
-     else
-       redirect to '/users'
-     end
+    post '/login' do
+      @user = User.find_by(username: params[:user][:username])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id]= @user.id
+        redirect to '/users/#{@user.slug}'
+      else
+        flash[:message]= "You have entered an incorrect username/password or have not yet signed up."
+        redirect to '/signup'
+      end
+    end
+
+    get '/users/:slug' do
+     @user= User.find_by_slug(params[:slug])
+     erb :'users/show'
    end
 
-  get '/users/:slug' do
-   @user =User.find_by_slug(params[:slug])
-   erb :'users/show'
- end
-
-  post '/users' do
-    if params[:user][:username]== "" || params[:user][:email]== "" || params[:user][:password]== ""
-      flash[:message] = "Please, fill in all the boxes."
-      redirect to '/signup'
-    else
-      @user = User.create(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
-      @user.save
-      session[:user_id]= @user.id
-       flash[:message] = "You have successfully signed up."
-      redirect to '/users'
+    get '/logout' do
+      if logged_in?
+        session.destroy
+        redirect to 'users/login'
+      else
+        redirect to '/'
+      end
     end
-  end
 
-  get '/login' do
-    if !logged_in?
-   erb :'users/login'
- else
-   redirect '/users'
- end
-end
-
-post '/login' do
-  user = User.find_by(username: params[:user][:username])
-  if user && user.authenticate(params[:user][:password])
-    session[:user_id]= user.id
-    redirect to '/users/#{user.slug}'
-  else
-    flash[:message]= "You have entered an incorrect username/password or have not yet signed up."
-    redirect to '/signup'
-  end
-end
 
     get '/users/:slug/edit' do
       if logged_in?
         @user = User.find_by_slug(params[:slug])
         if @user = current_user
-          erb :"/users/edit"
+          erb :"users/edit"
         else
           flash[:message] = "You do not have permission to edit someone else's profile"
           redirect to '/users'
@@ -70,22 +80,12 @@ end
       end
     end
 
-    patch '/users/:slug/edit' do
-      if logged_in?
+    patch "/users/:slug" do
         @user = User.find_by_slug(params[:slug])
-        @user.update(username: params[:user][:username], email: params[:user][:email], password: [:user][:password])
+        @user.update(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
         flash[:message]= "You have successfully updated your profile"
-        redirect to '/users/#{@user.slug}'
+        redirect to "users/#{@user.slug}"
       end
-
-    get '/logout' do
-      if logged_in?
-        session.destroy
-        redirect to '/login'
-      else
-        redirect to '/'
-      end
-    end
 
     get '/users/:slug/delete' do
       if logged_in?
@@ -103,6 +103,4 @@ end
       redirect to '/login'
     end
   end
-end
-
 end
