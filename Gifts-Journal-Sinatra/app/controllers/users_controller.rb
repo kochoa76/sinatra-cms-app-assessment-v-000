8c43492da38d5c65b9093e6a9f884c17ff439076
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-28 15:53:26 +0000

    got logout view working

--- Gifts-Journal-Sinatra/app/controllers/users_controller.rb
+++ Gifts-Journal-Sinatra/app/controllers/users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
     get '/logout' do
       if logged_in?
         session.destroy
-        redirect to 'users/login'
+        redirect to '/login'
       else
         redirect to '/'
       end
